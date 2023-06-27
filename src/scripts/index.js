0768e1a5d31587f5ea2adb04ba0a299cc504eace
chndrwali <chndrwali@gmail.com>
2023-06-27 15:48:07 +0700

    add header & footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/no-unresolved */
 import 'regenerator-runtime';
+import '../styles/style.css';
+import './views/components/header-logo';
+import './views/components/footer-brand';
 
 console.log('--------------------------------');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false,
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 };
